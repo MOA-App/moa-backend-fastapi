aa4a3d53527d8c3a3009ffe92b5868c9fb0372a4
sophiasussa <sophiasussa@outlook.com>
2026-04-14 08:45:43 -0300

    tests(auth): add role use cases tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 import pytest
 from unittest.mock import AsyncMock
 
+
+# ============================
+# PERMISSION REPOSITORY
+# ============================
+
 @pytest.fixture
 def mock_permission_repository():
     repo = AsyncMock()
@@ -10,6 +15,25 @@ def mock_permission_repository():
     repo.find_by_id = AsyncMock()
     repo.list_all = AsyncMock()
     repo.list_resources = AsyncMock()
+    repo.update = AsyncMock()
+    repo.delete = AsyncMock()
+
+    return repo
+
+
+# ============================
+# ROLE REPOSITORY
+# ============================
+
+@pytest.fixture
+def mock_role_repository():
+    repo = AsyncMock()
+
+    repo.create = AsyncMock()
+    repo.exists_by_name = AsyncMock(return_value=False)
+    repo.find_by_id = AsyncMock()
+    repo.find_by_name = AsyncMock()
+    repo.list_all = AsyncMock()
     repo.update = AsyncMock()
     repo.delete = AsyncMock()
 
